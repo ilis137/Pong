2cdfbf7f9ecc5f8afd7bfb6d6fff8ae17b9ccc35
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-08 06:44:31 +0530

    scores added and logic improved

--- pong.js
+++ pong.js
@@ -10,31 +10,35 @@ var ball = {
     x: 100,
     y: 200,
     r: 10,
-    dx: 5,
-    dy: 3
+    dx: 7,
+    dy: 5
 }
 var bat = [];
 bat[0] = {
     x: 0,
     y: 300,
     width: 10,
-    height: 100
+    height: 150
 };
 
 bat[1] = {
     x: window.innerWidth - 10,
     y: 300,
     width: 10,
-    height: 100,
-    dy: 50
+    height: 150,
+    dy: 60
 }
 myScore = 0;
 compScore = 0;
 compTurn = true;
+gameOver = false;
 
 function init() {
     cvs = document.querySelector("canvas");
     ctx = cvs.getContext("2d")
+    ctx.font = '48px serif';
+    ctx.fillText(myScore, 300, 100);
+    ctx.fillText(compScore, 900, 100);
 
     cvs.fillStyle = "black";
     cvs.width = window.innerWidth
@@ -48,72 +52,121 @@ function init() {
     ctx.lineTo(window.innerWidth / 2, window.innerHeight);
     ctx.stroke();
 
+    ball.x = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+    ball.y = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
     ctx.beginPath();
     ctx.fillStyle = "red";
-    ctx.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI, false);
+    ctx.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI, false); //ball
     ctx.fill();
 
     ctx.beginPath();
     ctx.fillStyle = "white";
-    ctx.fillRect(bat[0].x, bat[0].y, bat[0].width, bat[0].height)
+    bat[0].y = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+    bat[1].y = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+    ctx.fillRect(bat[0].x, bat[0].y, bat[0].width, bat[0].height) //bat1
 
-    ctx.fillRect(bat[1].x, bat[1].y, bat[1].width, bat[1].height)
+    ctx.fillRect(bat[1].x, bat[1].y, bat[1].width, bat[1].height) //bat2
+
     cvs.addEventListener("mousemove", function(e) {
         bat[0].y = e.offsetY
-        console.log(e)
+
+    })
+    cvs.addEventListener("click", function(e) {
+        if (compScore == 3 || myScore == 3)
+            console.log("in click")
+        gameOver = true;
+
+
     })
 }
 
+
 function animate() {
+
     requestAnimationFrame(animate);
 
-    ctx.setLineDash([10, 10]);
-    ctx.beginPath();
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 5;
-    ctx.moveTo(window.innerWidth / 2, 0);
-    ctx.lineTo(window.innerWidth / 2, window.innerHeight);
-    ctx.stroke();
-
     ctx.clearRect(0, 0, (window.innerWidth), (window.innerHeight));
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI, false);
-    ctx.fill();
+    console.log("in animate")
+    if (gameOver == true) {
 
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(bat[0].x, bat[0].y, bat[0].width, bat[0].height)
+        compScore = 0;
+        myScore = 0;
+        gameOver = false;
+    } else if (myScore == 3) {
+        ctx.font = '70px monospace';
+        ctx.textAlign = "center"
+        ctx.fillText("You Won", window.innerWidth / 2, 400);
+
+        ctx.font = '30px monospace';
+        ctx.textAlign = "center"
+        ctx.fillText("Click to continue", window.innerWidth / 2, 550);
+    } else if (compScore == 3) {
+        ctx.font = '70px monospace';
+        ctx.textAlign = "center"
+        ctx.fillText("Computer Won", window.innerWidth / 2, 400);
+
+        ctx.font = '30px monospace';
+        ctx.textAlign = "center"
+        ctx.fillText("Click to continue", window.innerWidth / 2, 550);
+
+    } else {
+        ctx.setLineDash([10, 10]);
+        ctx.beginPath();
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 5;
+        ctx.moveTo(window.innerWidth / 2, 0);
+        ctx.lineTo(window.innerWidth / 2, window.innerHeight);
+        ctx.stroke();
 
 
-    ctx.fillRect(bat[1].x, bat[1].y, bat[1].width, bat[1].height)
-    ball.x = ball.x + ball.dx;
+        ctx.font = '150px monospace';
+        ctx.fillText(myScore, window.innerWidth / 4, 200);
+        ctx.fillText(compScore, window.innerWidth * 0.75, 200);
 
-    ball.y = ball.y + ball.dy;
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI, false);
+        ctx.fill();
 
-    if (ball.x - ball.r <= bat[0].width && ball.y >= bat[0].y && ball.y <= bat[0].y + bat[0].height || bat[1].x <= ball.x + ball.r && ball.y >= bat[1].y && ball.y <= bat[1].y + bat[1].height) { ball.dx = -ball.dx; }
-    if (ball.y + ball.r >= window.innerHeight || ball.y - ball.r < 0) { ball.dy = -ball.dy; }
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.fillRect(bat[0].x, bat[0].y, bat[0].width, bat[0].height)
 
-    if (ball.y > bat[1].y + bat[1].height / 2 && compTurn == true)
-        bat[1].y += bat[1].dy;
-    if (ball.y < bat[1].y + bat[1].height / 2 && compTurn == true)
-        bat[1].y -= bat[1].dy;
 
-    if (ball.x - ball.r <= bat[0].width) {
-        compTurn = true;
-        console.log("no compturn")
+        ctx.fillRect(bat[1].x, bat[1].y, bat[1].width, bat[1].height)
+        ball.x = ball.x + ball.dx;
 
+        ball.y = ball.y + ball.dy;
+
+        if (ball.x - ball.r <= bat[0].width && ball.y >= bat[0].y && ball.y <= bat[0].y + bat[0].height || bat[1].x <= ball.x + ball.r && ball.y >= bat[1].y && ball.y <= bat[1].y + bat[1].height) { ball.dx = -ball.dx; }
+        if (ball.y + ball.r >= window.innerHeight || ball.y - ball.r < 0) { ball.dy = -ball.dy; }
+
+        if (ball.y > bat[1].y + bat[1].height / 2 && compTurn == true)
+            bat[1].y += bat[1].dy;
+        if (ball.y < bat[1].y + bat[1].height / 2 && compTurn == true)
+            bat[1].y -= bat[1].dy;
+
+        if (ball.x - ball.r <= bat[0].width) {
+            compTurn = true;
+            console.log("no compturn")
+
+        }
+        if (bat[1].x <= ball.x + ball.r) {
+            console.log(" compturn")
+            compTurn = false;
+        }
+        if (ball.x > window.innerWidth || ball.x < 0) {
+            ball.x = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+            ball.y = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+            ball.dx = -ball.dx
+            compTurn = true;
+
+            ball.x > window.innerWidth ? myScore++ : compScore++
+        }
     }
-    if (bat[1].x <= ball.x + ball.r) {
-        console.log(" compturn")
-        compTurn = false;
-    }
-    if (ball.x > window.innerWidth || ball.x < 0) {
-        ball.x = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
-        ball.y = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
-        /* if (dx > 0)
-             compTurn = true;*/
-    }
+
+
+
 }
 
 
